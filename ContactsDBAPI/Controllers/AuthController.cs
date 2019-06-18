ded7ad15642c2fe6e20d07d66b2ee23c207c7126
etnicholson@gmail.com <etnicholson@gmail.com>
2019-06-18 10:22:47 -0400

    login and register

--- ContactsDBAPI/Controllers/AuthController.cs
+++ ContactsDBAPI/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using ContactsDBAPI.Dto;
 using ContactsDBAPI.Models;
@@ -8,6 +11,7 @@ using ContactsDBAPI.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ContactsDBAPI.Controllers
 {
@@ -41,6 +45,45 @@ namespace ContactsDBAPI.Controllers
             var userToReturn = createdUser;
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserForRegisterDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Email.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Email, userFromRepo.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes("NE193SXuWSXae7OZLSLLFlHzk9HlcwOnFMMNOYqaIXQKz24VOpmjHzV20WPuOqR"));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(10),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            //var user = _mapper.Map<UserForListDto>(userFromRepo);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token),
+                userForLoginDto.Email
+            });
         }
 
     }
