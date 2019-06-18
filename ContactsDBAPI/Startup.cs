4e58db03541d913223892d941ff02299fbbd15d7
etnicholson@gmail.com <etnicholson@gmail.com>
2019-06-18 09:21:33 -0400

    added Cors

--- ContactsDBAPI/Startup.cs
+++ ContactsDBAPI/Startup.cs
@@ -25,6 +25,15 @@ namespace ContactsDBAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    );
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -38,11 +47,15 @@ namespace ContactsDBAPI
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+
+
+            app.UseCors("CorsPolicy");
+            //app.UseHttpsRedirection();
             app.UseMvc();
+
         }
     }
 }
